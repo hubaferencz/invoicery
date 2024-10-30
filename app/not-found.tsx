7cb795c2511d772ddd419dfe23574ced9ca3f37c
swikko-com <huba.ferencz.personal@gmail.com>
2024-10-30 21:01:30 +0100

    a

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import HeroCta from "./(hero)/HeroCta";
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-full px-4 text-white min-h-96 bg-primary-700 py-52 flex-col gap-10">
+      <h1 className="text-[32px] lg:text-[56px] font-bold leading-tight">
+        Page not found 👀
+      </h1>
+      <p className="text-base font-semibold leading-normal lg:text-lg">
+        The page you are looking for does not exist
+      </p>
+      <HeroCta text={"Return to home page"} link="/" />
+    </div>
+  );
+}
