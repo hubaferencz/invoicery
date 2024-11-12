86ce8796bd4e8e388e47ae804125b9659daea246
swikko-com <huba.ferencz.personal@gmail.com>
2024-11-12 07:08:01 +0100

    a

---
+++ app/dashboard/components/tasks/assignment/forms/customer/Info.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = { title: string; description: string };
+
+export default function Info({ title, description }: Props) {
+  return (
+    <div className="w-full select-none cursor-pointer border border-[#CBE3EF] p-3 flex rounded-[9px] items-center justify-start bg-[#EEF5F8]">
+      <div className="items-start justify-start gap-3 flex">
+        <Image src={"/icons/info.svg"} width={20} height={20} alt="info" />
+        <div className="flex flex-col items-start justify-center gap-1">
+          <h3
+            className="text-sm font-semibold"
+            style={{ letterSpacing: "0.14px" }}
+          >
+            {title}
+          </h3>
+          <p
+            className="text-xs leading-normal"
+            style={{ letterSpacing: "0.12px" }}
+          >
+            {description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
